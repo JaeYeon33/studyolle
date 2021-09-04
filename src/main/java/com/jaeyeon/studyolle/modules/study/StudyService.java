3fd9acf5596805d5ea4af974d20de35f65127a5d
JaeYeon33 <cjyeon1022@gmail.com>
2021-09-05 03:43:34 +0900

    2021-09-05 알림 인프라 설정

--- src/main/java/com/jaeyeon/studyolle/modules/study/StudyService.java
+++ src/main/java/com/jaeyeon/studyolle/modules/study/StudyService.java
@@ -1,11 +1,13 @@
 package com.jaeyeon.studyolle.modules.study;
 
 import com.jaeyeon.studyolle.modules.account.Account;
+import com.jaeyeon.studyolle.modules.study.event.StudyCreatedEvent;
 import com.jaeyeon.studyolle.modules.study.form.StudyDescriptionForm;
 import com.jaeyeon.studyolle.modules.tag.Tag;
 import com.jaeyeon.studyolle.modules.zone.Zone;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,10 +21,12 @@ public class StudyService {
 
     private final StudyRepository studyRepository;
     private final ModelMapper modelMapper;
+    private final ApplicationEventPublisher eventPublisher;
 
     public Study createNewStudy(Study study, Account account) {
         Study newStudy = studyRepository.save(study);
         newStudy.addManager(account);
+        eventPublisher.publishEvent(new StudyCreatedEvent(newStudy));
         return newStudy;
     }
 
---
+++ src/main/java/com/jaeyeon/studyolle/modules/study/event/StudyCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.jaeyeon.studyolle.modules.study.event;
+
+import com.jaeyeon.studyolle.modules.study.Study;
+import lombok.Getter;
+
+@Getter
+public class StudyCreatedEvent {
+
+    private Study study;
+
+    public StudyCreatedEvent(Study study) {
+        this.study = study;
+    }
+}

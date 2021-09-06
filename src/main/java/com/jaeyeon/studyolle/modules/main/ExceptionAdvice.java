0fdddbd3a27d68b370c0f5340abf5bafdf18de69
JaeYeon33 <cjyeon1022@gmail.com>
2021-09-06 23:45:31 +0900

    2021-09-06 에러 핸들러 및 뷰 추가

---
+++ src/main/java/com/jaeyeon/studyolle/modules/main/ExceptionAdvice.java
@@ -0,0 +1,26 @@
+package com.jaeyeon.studyolle.modules.main;
+
+import com.jaeyeon.studyolle.modules.account.Account;
+import com.jaeyeon.studyolle.modules.account.CurrentUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler
+    public String handleRuntimeException(@CurrentUser Account account, HttpServletRequest request, RuntimeException e) {
+        if (account != null) {
+            log.info("'{}' requested '{}'", account.getNickname(), request.getRequestURI());
+        } else {
+            log.info("requested '{}'", request.getRequestURI());
+        }
+
+        log.error("bad request", e);
+        return "error";
+    }
+}
